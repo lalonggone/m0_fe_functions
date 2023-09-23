eb912e8a4157e4029d29cecf9050cd0eea65fa36
Laura Long <lauralonggone@gmail.com>
2023-09-22 22:09:14 -0600

    Add comments of changes made to all examples

--- cleanUp.js
+++ cleanUp.js
@@ -8,33 +8,46 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
-
-askForName()
+  // Okay so this one seems really messed up to me.. I just made it look like a regular function declaration.
+  // Kept the function keyword b/c its descriptive
+  // Changed console.log... to -> return "Hello, what is your name?"; and put it on the next line. No parentheses.
+  // Added curly brace on next line and console.log(askForName()) on the line after that
+  function askForName() { 
+    return "Hello, what is your name?";
+  }
+  console.log(askForName())
 
 
 // EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
-
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+  // Yikes this one was messy, too. Not sure if I found everything but the code runs so..
+  // Indented var and return for better readability.
+  // Added a semicolon after the var sum
+  // Wrapped the two functions(?) in separate console.logs to print the sum
+  function addThreeNums(first, second, third) {
+    var sum = first + second + third;
+    return sum;
+  }
+  
+  
+  console.log(addThreeNums(1, 2, 3))
+  console.log(addThreeNums(4, 2, 7))
 
 
 // EX 3:
-func makeFreshPesto(){
+// I changed "func" to "function" because func is not a command.. and moved the last curly brace down
+function makeFreshPesto(){
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
 
 //  EX 4:
+  // I'm not gettin this one..
 function average(num1, num2) 
   {
 var sum = num1 + num2;
